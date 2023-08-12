cb6eb67967b83b7669e7880151d267970101e6f1
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-12 10:39:11 +0200

    clap

---
+++ src/models/args.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Latitude
+    #[arg(short = 't', long, value_name = "Latitude", allow_negative_numbers = true)]
+    pub latitude: String,
+    
+    /// Longitude
+    #[arg(short = 'n', long, value_name = "Longitude", allow_negative_numbers = true)]
+    pub longitude: String,
+
+    /// Timezone
+    #[arg(short = 'z', long, value_name = "Timezone", default_value = "Europe/Madrid")]
+    pub timezone: String,
+}
+
+fn default_timezone() -> String{
+    "Europe/Madrid".to_string()
+}
+
