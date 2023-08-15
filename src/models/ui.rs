db66e0797c70f9454e714a4dd2f3f7c73fd02b51
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-15 20:20:20 +0200

    :wrench: blocking

--- src/models/ui.rs
+++ src/models/ui.rs
@@ -9,18 +9,21 @@ use ratatui::{
 use crate::models::app::App;
 
 /// Renders the user interface widgets.
-pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
+pub fn render<B: Backend>(app: &mut App<'_>, frame: &mut Frame<'_, B>) {
     // This is where you add new widgets.
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/tui-rs-revival/ratatui/tree/master/examples
+    let data = app.omc.get().unwrap();
     frame.render_widget(
         Paragraph::new(format!(
             "This is a tui template.\n\
                 Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
                 Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            app.counter
+                Counter: {}.\n\
+                Data: {:?}",
+            app.counter,
+            data
         ))
         .block(
             Block::default()
