725d0e80a3d3e87c58d3b84323879d18d1c2f6df
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-08-17 06:43:18 +0200

    :sparkles: table

--- src/models/ui.rs
+++ src/models/ui.rs
@@ -2,7 +2,7 @@ use ratatui::{
     prelude::*,
     backend::Backend,
     layout::Alignment,
-    widgets::{Block, Borders, Paragraph, Wrap},
+    widgets::{Block, Borders, Paragraph, Wrap, Table, Row, Cell},
     Frame,
 };
 use itertools::Itertools;
@@ -18,7 +18,7 @@ pub fn render<B: Backend>(app: &mut App<'_>, frame: &mut Frame<'_, B>) {
 
     let paragraph = placeholder_paragraph();
 
-    let data = app.omc.get().unwrap();
+    //let data = app.omc.get().unwrap();
 
     render_borders(&paragraph, Borders::ALL, frame, layout[0][0]);
     render_borders(&paragraph, Borders::NONE, frame, layout[0][1]);
@@ -26,6 +26,7 @@ pub fn render<B: Backend>(app: &mut App<'_>, frame: &mut Frame<'_, B>) {
     render_borders(&paragraph, Borders::RIGHT, frame, layout[1][1]);
     render_borders(&paragraph, Borders::TOP, frame, layout[2][0]);
     render_borders(&paragraph, Borders::BOTTOM, frame, layout[2][1]);
+    render_table(frame, layout[3][0]);
 }
 
 /// Calculate the layout of the UI elements.
@@ -39,7 +40,7 @@ fn calculate_layout(area: Rect) -> (Rect, Vec<Vec<Rect>>) {
     let title_area = layout[0];
     let main_areas = Layout::default()
         .direction(Direction::Vertical)
-        .constraints(vec![Constraint::Max(4); 9])
+        .constraints(vec![Constraint::Max(10); 9])
         .split(layout[1])
         .iter()
         .map(|&area| {
@@ -73,4 +74,48 @@ fn render_borders<B: Backend>(paragraph: &Paragraph, border: Borders, frame: &mu
     frame.render_widget(paragraph.clone().block(block), area);
 }
 
-
+fn render_table<B: Backend>(frame: &mut Frame<'_, B>, area: Rect) {
+    let table = Table::new(vec![
+        // Row can be created from simple strings.
+        Row::new(vec!["Row11", "Row12", "Row13"]),
+        // You can style the entire row.
+        Row::new(vec!["Row21", "Row22", "Row23"]).style(Style::default().fg(Color::Blue)),
+        // If you need more control over the styling you may need to create Cells directly
+        Row::new(vec![
+            Cell::from("Row31"),
+            Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
+            Cell::from(Line::from(vec![
+                Span::raw("Row"),
+                Span::styled("33", Style::default().fg(Color::Green))
+            ])),
+        ]),
+        // If a Row need to display some content over multiple lines, you just have to change
+        // its height.
+        Row::new(vec![
+            Cell::from("Row\n41"),
+            Cell::from("Row\n42"),
+            Cell::from("Row\n43"),
+        ]).height(2),
+    ])
+        // You can set the style of the entire Table.
+        .style(Style::default().fg(Color::White))
+        // It has an optional header, which is simply a Row always visible at the top.
+        .header(
+            Row::new(vec!["Col1", "Col2", "Col3"])
+                .style(Style::default().fg(Color::Yellow))
+                // If you want some space between the header and the rest of the rows, you can always
+                // specify some margin at the bottom.
+                .bottom_margin(1)
+        )
+        // As any other widget, a Table can be wrapped in a Block.
+        .block(Block::default().borders(Borders::ALL).title("Table"))
+        // Columns widths are constrained in the same way as Layout...
+        .widths(&[Constraint::Length(5), Constraint::Length(5), Constraint::Length(10)])
+        // ...and they can be separated by a fixed spacing.
+        .column_spacing(1)
+        // If you wish to highlight a row in any specific way when it is selected...
+        .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+        // ...and potentially show a symbol in front of the selection.
+        .highlight_symbol(">>");
+    frame.render_widget(table, area);
+}
